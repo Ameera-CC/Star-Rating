19f4bd7a1d24b9458b44843243a4238f705deef1
Ameera <ameeraccammu@gmail.com>
2024-01-23 15:00:37 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let htmlElement = '';
+
+let res;
+for(let i=0; i<5; i++){
+
+    htmlElement+=`<span class="css-star js-star">&#9733</span>`;
+}
+document.querySelector('.js-star-div').innerHTML = htmlElement
+
+
+let stars = document.querySelectorAll('.js-star')
+console.log(stars)
+
+stars.forEach((item,index) => {
+    item.addEventListener('click',() => {  
+        console.log('old index : ',index)
+
+        res = index+1;
+        stars.forEach((item, newindex) => {
+
+            if(index >= newindex){
+                console.log('new index : ',newindex)
+                item.classList.add('css-color')
+                document.querySelector('.js-rating-res').innerHTML = `Rating is : <span class="css-deflt-txt"> ${res} </span>  /  5 `;
+            }
+            else{
+                console.log('else part')
+                item.classList.remove('css-color')
+            }
+
+        })
+    })
+})
+
